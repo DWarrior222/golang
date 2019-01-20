2dd4523337063a96ac242dec8a8907283dc9494c
guanshuaijie <1845543324@qq.com>
2019-01-20 21:45:45 +0800

    hello.go

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+	// var num = 20
+	// fmt.Println("姓名\t 年龄\t 籍贯\t 地址\njohn\t20\t 河北\t 北京")
+	/* */
+	// fmt.Sprintf("%[2]d %[1]d\n", 11, num)
+	fmt.Println("姓名\t 年龄\t 籍贯\t 地址\njohn\t 20\t 河北\t 北京")
+	// fmt.Sprintf("%[3]*.[2]*[1]f", 12.0, 2, 6)
+}
